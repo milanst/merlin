cf237ee31b4ac3a479f41cd61c45aeab68689adb
Frédéric Bour <frederic.bour@lakaban.net>
2015-01-22 14:52:40 +0100

    Merlin_typer: compute typemap from closest env

--- src/kernel/merlin_typer.mli
+++ src/kernel/merlin_typer.mli
@@ -41,4 +41,4 @@ val extensions : t -> Extension.set
 val dump : Format.formatter -> t -> unit
 
 val with_typer : t -> (unit -> 'a) -> 'a
-val typemap : t -> Printtyp.typemap
+val typemap : ?from:Env.t -> t -> Printtyp.typemap
