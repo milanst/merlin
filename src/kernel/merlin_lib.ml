310a6f09c06f506b8adbeb7cd35b1af47a9510ff
Thomas Refis <thomas.refis@gmail.com>
2015-01-30 15:23:21 +0000

    ocamldoc: naive binding

--- src/kernel/merlin_lib.ml
+++ src/kernel/merlin_lib.ml
@@ -425,6 +425,7 @@ module Buffer : sig
   type t
   val create: ?dot_merlins:string list -> ?path:string -> Parser.state -> t
 
+  val unit_name : t -> string
   val project: t -> Project.t
 
   val lexer: t -> (exn list * Lexer.item) History.t
@@ -531,6 +532,8 @@ end = struct
       | None -> ()
     end;
     Project.setup buffer.project
+
+  let unit_name t = t.unit_name
 
   let project t = t.project
 
